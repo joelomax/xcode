3e385b0d5877ae3138d642c0401039c70bdd82ae
codenation <codenation@codenations-MacBook-Pro.local>
2019-06-28 15:43:00 +0100

    1st commit

---
+++ CoffeeShop/CoffeeShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeShop
+//
+//  Created by codenation on 19/06/2019.
+//  Copyright © 2019 codenation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testdemo/testdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testdemo
+//
+//  Created by codenation on 18/06/2019.
+//  Copyright © 2019 codenation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
